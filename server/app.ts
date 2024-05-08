aa079b2a681d7943259165ce0be458911efd5635
C-Mierez <carlos.mierez20@gmail.com>
2024-05-08 14:18:46 -0300

    ✨ Serve static files

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,19 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 import { logger } from "hono/logger";
 import { expensesRoutes } from "~/routes/expenses";
 
 const app = new Hono();
 
+/* ------------------------------- Middleware ------------------------------- */
 // Set up Hono's built-in request logger middleware
 app.use("*", logger());
 
-app.get("/test", (c) => {
-    return c.json({ message: "test" });
-});
-
+/* --------------------------------- Routes --------------------------------- */
 app.route("api/expenses", expensesRoutes);
+
+/* --------------------------------- Static --------------------------------- */
+app.use("*", serveStatic({ root: "./client/dist" }));
+app.use("*", serveStatic({ path: "./client/dist/index.html" }));
 
 export default app;
