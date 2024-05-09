9d9a9dac625747ac023871cec5dcb33b50a9cc98
C-Mierez <carlos.mierez20@gmail.com>
2024-05-09 15:32:26 -0300

    ✨ Added Tanstack Query provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "~/styles/index.css";
+
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+
+import App from "./App.tsx";
+import QueryProvider from "./providers/query-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <QueryProvider>
+            <App />
+        </QueryProvider>
     </React.StrictMode>
 );
