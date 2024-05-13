fed464f86db2a636bf8d5d2e485f2e3fd124317f
C-Mierez <carlos.mierez20@gmail.com>
2024-05-13 20:11:00 -0300

    📦 Tanstack Router

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,11 @@
 import path from "path";
-import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), TanStackRouterVite()],
     resolve: {
         alias: {
             "~": path.resolve(__dirname, "./src"),
